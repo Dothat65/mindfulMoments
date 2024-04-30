493c3b501f011f02022f61ef65ad9248d9e19a9e
Daniel Nadeem <Danielnadeem29@gmail.com>
2024-04-30 18:46:56 -0400

    Added "!"

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Welcome to My React App</h1>
+        <h1>Welcome to My React App!!!!!!!!!!!!!!!!!!!!!!!!</h1>
       </header>
       <main>
         <p>This is a simple React application.</p>
