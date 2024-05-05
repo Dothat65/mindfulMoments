d66b681d0ca678b9ba162719abbf24635603b2ee
Daniel Nadeem <Danielnadeem29@gmail.com>
2024-05-05 16:19:30 -0400

    working on journal page

--- src/routes/journal.jsx
+++ src/routes/journal.jsx
@@ -2,8 +2,22 @@ import React from 'react';
 
 function Journal() {
     return (
-        <div>
-        <h1>Journal</h1>
+                <div className="journal-container">
+            <h1 className="journal-heading">Journal</h1>
+            <textarea
+                className="journal-textarea"
+                placeholder="Write your journal entry here..."
+            ></textarea>
+            <button className="post-button">Post</button>
+
+            {/* Display journal entries */}
+            <div className="entry-container">
+                <div>
+                    <h3 className="entry-date">05/05/2024</h3>
+                    <p className="entry-content">This is a sample journal entry.</p>
+                </div>
+                {/* Add more entries here */}
+            </div>
         </div>
     );
     }
